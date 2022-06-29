51697fce1658fb85b383137029c8471251655b14
benpankow <ben@elementl.com>
2022-06-29 14:33:43 -0700

    comment on post

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,19 +8,24 @@ EMAIL=$(git log -1 --format='%ae')
 NAME=$(git log -1 --format='%an')
 PR_NUMBER=$1
 PR_URL="https://github.com/${GITHUB_REPOSITORY}/pull/${PR_NUMBER}"
-if [ -z $PR_NUMBER ]
-then
-    PR_URL=""
-fi
+COMMENTS_URL="${PR_URL}/comments"
+
 dagster-cloud branch-deployment create-or-update \
     --url https://7151-136-24-32-204.ngrok.io/1/prod \
     --api-token "agent:test:hardcoded" \
     --git-repo-name "$GITHUB_REPOSITORY" \
     --branch-name "$GITHUB_HEAD_REF" \
-    --branch-url "https://github.com/${GITHUB_REPOSITORY}/tree/${GITHUB_REF_NAME}" \
+    --branch-url "https://github.com/${GITHUB_REPOSITORY}/tree/${GITHUB_HEAD_REF}" \
     --pull-request-url "$PR_URL" \
     --commit-hash "$GITHUB_SHA" \
     --timestamp "$TIMESTAMP" \
     --commit-message "$MESSAGE" \
     --author-name "$NAME" \
     --author-email "$EMAIL"
+
+curl \
+    -X POST \
+    $COMMENTS_URL \
+    -H "Content-Type: application/json" \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    --data '{ "body": "blah blah" }'

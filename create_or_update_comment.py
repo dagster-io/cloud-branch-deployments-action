e9009537b7c74cd2e7bc56c320ae09c1cddbd764
benpankow <ben@elementl.com>
2022-06-29 15:45:31 -0700

    remove reliance on current user

--- create_or_update_comment.py
+++ create_or_update_comment.py
@@ -8,20 +8,18 @@ pr_id = int(os.getenv("INPUT_PR"))
 repo_id = os.getenv("GITHUB_REPOSITORY")
 action = os.getenv("INPUT_ACTION")
 
-current_user = g.get_user()
 
 repo = g.get_repo(repo_id)
 pr = repo.get_pull(pr_id)
 
 comments = pr.get_issue_comments()
 comment_to_update = None
-# for comment in comments:
-#     if comment.user.login == current_user.login:
-#         comment_to_update = comment
-#         break
+for comment in comments:
+    if comment.user.login == "github-actions[bot]" and "Dagster Cloud" in comment.body:
+        comment_to_update = comment
+        break
 
-message = f"{action} :sunglasses:"
-
+message = f"Your pull request is automatically being deployed to Dagster Cloud. {action}"
 
 if comment_to_update:
     comment_to_update.edit(message)

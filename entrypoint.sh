ada8262b3245aa8cff9f9528c51e6ad8808c6621
benpankow <ben@elementl.com>
2022-06-29 12:10:53 -0700

    add create-or-update call

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,15 @@ echo "::set-output name=time::$time"
 help=$(dagster-cloud branch-deployment create-or-update --help)
 echo "::set-output name=help::$help"
 
-env
+cd /github/home
+
+TIMESTAMP=$(git log -1 --format=%cd --date=unix)
+echo $TIMESTAMP
+
+dagster-cloud-branch-deployment create-or-update \
+    --url https://7151-136-24-32-204.ngrok.io/1/prod/ \
+    --api-token "agent:test:hardcoded" \
+    --git-repo-name $GITHUB_REPOSITORY \
+    --branch-name $GITHUB_REF_NAME \
+    --commit-hash $GITHUB_SHA \
+    --timestamp $TIMESTAMP

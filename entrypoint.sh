a6b8c560b3705747621f6e46fa619274fa5b0ea7
benpankow <ben@elementl.com>
2022-06-29 12:55:56 -0700

    pr url

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,12 +13,19 @@ TIMESTAMP=$(git log -1 --format='%cd' --date=unix)
 MESSAGE=$(git log -1 --format='%s')
 EMAIL=$(git log -1 --format='%ae')
 NAME=$(git log -1 --format='%an')
+PR_NUMBER=$(gh pr view --json number -q .number)
+PR_URL="https://github.com/${GITHUB_REPOSITORY}/pull/${PR_NUMBER}"
+if [ $? -eq 1 ]
+then
+    PR_URL=""
+fi
 dagster-cloud branch-deployment create-or-update \
     --url https://7151-136-24-32-204.ngrok.io/1/prod \
     --api-token "agent:test:hardcoded" \
     --git-repo-name "$GITHUB_REPOSITORY" \
     --branch-name "$GITHUB_REF_NAME" \
     --branch-url "https://github.com/${GITHUB_REPOSITORY}/tree/${GITHUB_REF_NAME}" \
+    --pull-request-url "$PR_URL" \
     --commit-hash "$GITHUB_SHA" \
     --timestamp "$TIMESTAMP" \
     --commit-message "$MESSAGE" \

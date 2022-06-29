265e3b0481198c7544a0c033c2d2c662c177bb9b
benpankow <ben@elementl.com>
2022-06-29 16:07:06 -0700

    daggy image

--- create_or_update_comment.py
+++ create_or_update_comment.py
@@ -17,15 +17,33 @@ for comment in comments:
         comment_to_update = comment
         break
 
-SUCCESS_IMAGE = "![daggy](https://raw.githubusercontent.com/dagster-io/dagster/9842ba843d281146ab0242be96e2c6301f5e42c9/js_modules/dagit/packages/app/public/favicon-run-success.svg | width=100 height=100)"
+SUCCESS_IMAGE = """
+<img
+    alt="build succeeded"
+    src="https://raw.githubusercontent.com/dagster-io/dagster/9842ba843d281146ab0242be96e2c6301f5e42c9/js_modules/dagit/packages/app/public/favicon-run-success.svg"
+    width=25
+    height=25
+/>
+"""
+
+BUILDING_IMAGE = """
+<img
+    alt="build succeeded"
+    src="https://raw.githubusercontent.com/dagster-io/dagster/9842ba843d281146ab0242be96e2c6301f5e42c9/js_modules/dagit/packages/app/public/favicon-run-pending.svg"
+    width=25
+    height=25
+/>
+"""
 
 deployment_url = f"https://7151-136-24-32-204.ngrok.io/1/{deployment_name}/"
+
+status_image = SUCCESS_IMAGE if action == "complete" else BUILDING_IMAGE
 message = f"""
 Your pull request is automatically being deployed to Dagster Cloud.
 
 | Location      | Status          | Link              |
 | ------------- | --------------- | ----------------- |
-| `my_location` | {SUCCESS_IMAGE} | {deployment_url}  |
+| `my_location` | {status_image}  | {deployment_url}  |
 """
 
 if comment_to_update:

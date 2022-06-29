ad8bf754452271527095d639fa9088a881b49efb
benpankow <ben@elementl.com>
2022-06-29 12:23:55 -0700

    where am i

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ echo "::set-output name=time::$time"
 help=$(dagster-cloud branch-deployment create-or-update --help)
 echo "::set-output name=help::$help"
 
-cd /github/home
+ls -la
 
 TIMESTAMP=$(git log -1 --format=%cd --date=unix)
 echo $TIMESTAMP
 
-dagster-cloud-branch-deployment create-or-update \
+dagster-cloud branch-deployment create-or-update \
     --url https://7151-136-24-32-204.ngrok.io/1/prod/ \
     --api-token "agent:test:hardcoded" \
     --git-repo-name $GITHUB_REPOSITORY \

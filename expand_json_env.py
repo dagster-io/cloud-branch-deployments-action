299ce734657c337d5e3b81d5a14719b28f458701
benpankow <ben@elementl.com>
2022-06-30 16:28:14 -0700

    pass location obj clean 2

--- expand_json_env.py
+++ expand_json_env.py
@@ -2,5 +2,5 @@ import json, os
 
 with open("json_env", mode="w") as f:
     for k, v in json.loads(os.getenv("INPUT_LOCATION")).items():
-        f.write(f"export LOCATION_{k.upper()}={v}")
+        f.write(f"LOCATION_{k.upper()}={v}\n")
 print("json_env")

3ae23b76db43bc5efdf277b29db566716f3a2376
benpankow <ben@elementl.com>
2022-06-30 16:39:38 -0700

    pass location obj clean 5

--- notify.sh
+++ notify.sh
@@ -1,12 +1,9 @@
-#!/bin/sh -
 #!/bin/bash -
 
 # Load JSON-encoded location info into env vars
 # This produces the env vars
 # LOCATION_NAME, LOCATION_LOCATION_FILE, LOCATION_REGISTRY
-source $(python expand_json_env.py); echo $LOCATION_NAME
-
-env
+source $(python expand_json_env.py)
 
 PR_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/pull/${INPUT_PR}"
 export GITHUB_RUN_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
@@ -16,5 +13,8 @@ COMMENTS_URL="${PR_URL}/comments"
 if [ -z $DAGSTER_CLOUD_URL ]; then
   export DAGSTER_CLOUD_URL="https://dagster.cloud/${INPUT_ORGANIZATION_ID}"
 fi
+
+echo $LOCATION_NAME
+echo "HMMMM"
 
 python create_or_update_comment.py

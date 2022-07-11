8589b47dfd8524ef4a4de200e7e4019e034d43d0
benpankow <ben@elementl.com>
2022-07-11 12:05:22 -0700

    Add run command

---
+++ src/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -
+
+# Load JSON-encoded location info into env vars
+# This produces the env vars
+# LOCATION_NAME, LOCATION_LOCATION_FILE, LOCATION_REGISTRY
+source $(python /expand_json_env.py)
+
+if [ -z $LOCATION_REGISTRY ]; then
+    LOCATION_REGISTRY="${!LOCATION_REGISTRY_ENV}"
+fi
+
+if [ -z $DAGSTER_CLOUD_URL ]; then
+    if [ -z $INPUT_DAGSTER_CLOUD_URL ]; then
+        export DAGSTER_CLOUD_URL="https://dagster.cloud/${INPUT_ORGANIZATION_ID}"
+    else
+        export DAGSTER_CLOUD_URL="${INPUT_DAGSTER_CLOUD_URL}"
+    fi
+fi
+
+dagster-cloud job launch \
+    --url "${DAGSTER_CLOUD_URL}/${INPUT_DEPLOYMENT}" \
+    --api-token "$DAGSTER_CLOUD_API_TOKEN" \
+    --location "${LOCATION_NAME}" \
+    --repostiory "${INPUT_REPOSITORY}"
+    --job "${INPUT_JOB}"
+    # --tags "${INPUT_TAGS_JSON}"
+    # --config-json "${INPUT_CONFIG_JSON}"

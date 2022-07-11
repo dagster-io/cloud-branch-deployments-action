81d65439d85f097c53990a7db3d4875a301b4479
benpankow <ben@elementl.com>
2022-07-11 08:46:26 -0700

    Fetch avatar URL from commit sourced from GitHub API

--- src/fetch_github_avatar.py
+++ src/fetch_github_avatar.py
@@ -9,15 +9,14 @@ Fetches a user's avatar from the Github API based on email or username
 def main():
     # Fetch various pieces of info from the environment
     g = Github(os.getenv("GITHUB_TOKEN"))
-    email = os.getenv("EMAIL")
-    name = os.getenv("NAME")
 
-    query = f"{email} OR {name}"
-    res = g.search_users(query=query)
-    if res.totalCount > 0:
-        print(res[0].avatar_url)
-    else:
-        print("")
+    repo_id = os.getenv("GITHUB_REPOSITORY")
+    commit_sha = os.getenv("GITHUB_SHA")
+
+    repo = g.get_repo(repo_id)
+    commit = repo.get_commit(commit_sha)
+
+    print(commit.author.avatar_url)
 
 
 if __name__ == "__main__":

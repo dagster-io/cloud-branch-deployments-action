54516bac4b88471604c4b8a77df00ab5c819ef09
benpankow <ben@elementl.com>
2022-07-20 14:06:47 -0700

    add comments

--- src/notify.sh
+++ src/notify.sh
@@ -4,6 +4,10 @@
 # This produces the env vars
 # INPUT_NAME, INPUT_LOCATION_FILE, INPUT_REGISTRY
 source $(python /expand_json_env.py)
+
+# The env var we get out of the `location` input is just `INPUT_NAME`
+# the env var we get out of the `location_name` input is `INPUT_LOCATION_NAME`
+# this just ensures we use whichever one is set
 if [ -z $INPUT_LOCATION_NAME ]; then
     INPUT_LOCATION_NAME="${INPUT_NAME}"
 fi
@@ -13,6 +17,7 @@ export GITHUB_RUN_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${
 
 COMMENTS_URL="${PR_URL}/comments"
 
+# Generate cloud URL, which might be directly supplied as env var or input, or generate from org ID
 if [ -z $DAGSTER_CLOUD_URL ]; then
   export DAGSTER_CLOUD_URL="https://dagster.cloud/${INPUT_ORGANIZATION_ID}"
 fi

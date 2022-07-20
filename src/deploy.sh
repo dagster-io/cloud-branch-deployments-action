559ac5c2207a43c5137e4869ae41877991fcc0cb
benpankow <ben@elementl.com>
2022-07-20 13:14:04 -0700

    Allow directly passing location info to actions

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,16 +2,15 @@
 
 # Load JSON-encoded location info into env vars
 # This produces the env vars
-# LOCATION_NAME, LOCATION_LOCATION_FILE, LOCATION_REGISTRY
+# INPUT_NAME, INPUT_LOCATION_FILE, INPUT_REGISTRY
 source $(python /expand_json_env.py)
+if [ -z $INPUT_LOCATION_NAME ]; then
+    INPUT_LOCATION_NAME=$INPUT_NAME
+else
 
 if [ -z $INPUT_REGISTRY ]; then
-    if [ -z $LOCATION_REGISTRY ]; then
-        LOCATION_REGISTRY="${!LOCATION_REGISTRY_ENV}"
-    fi
+    INPUT_REGISTRY="${!INPUT_REGISTRY_ENV}"
 else
-    LOCATION_REGISTRY="${INPUT_REGISTRY}"
-fi
 
 if [ -z $DAGSTER_CLOUD_URL ]; then
     if [ -z $INPUT_DAGSTER_CLOUD_URL ]; then
@@ -81,13 +80,13 @@ if [ -z $DEPLOYMENT_NAME ]; then
     exit 1
 fi
 
-echo "Deploying location ${LOCATION_NAME} to deployment ${DEPLOYMENT_NAME}..."
+echo "Deploying location ${INPUT_LOCATION_NAME} to deployment ${DEPLOYMENT_NAME}..."
 
 echo "::set-output name=deployment::${DEPLOYMENT_NAME}"
 
 dagster-cloud workspace add-location \
     --url "${DAGSTER_CLOUD_URL}/${DEPLOYMENT_NAME}" \
     --api-token "$DAGSTER_CLOUD_API_TOKEN" \
-    --location-file "${LOCATION_LOCATION_FILE}" \
-    --location-name "${LOCATION_NAME}" \
+    --location-file "${INPUT_LOCATION_FILE}" \
+    --location-name "${INPUT_LOCATION_NAME}" \
     --image "${LOCATION_REGISTRY}:${INPUT_IMAGE_TAG}"
--- src/notify.sh
+++ src/notify.sh
@@ -2,8 +2,11 @@
 
 # Load JSON-encoded location info into env vars
 # This produces the env vars
-# LOCATION_NAME, LOCATION_LOCATION_FILE, LOCATION_REGISTRY
+# INPUT_NAME, INPUT_LOCATION_FILE, INPUT_REGISTRY
 source $(python /expand_json_env.py)
+if [ -z $INPUT_LOCATION_NAME ]; then
+    INPUT_LOCATION_NAME=$INPUT_NAME
+else
 
 PR_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/pull/${INPUT_PR}"
 export GITHUB_RUN_URL="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
@@ -14,5 +17,5 @@ if [ -z $DAGSTER_CLOUD_URL ]; then
   export DAGSTER_CLOUD_URL="https://dagster.cloud/${INPUT_ORGANIZATION_ID}"
 fi
 
-export LOCATION_NAME=$LOCATION_NAME
+export INPUT_LOCATION_NAME=$INPUT_LOCATION_NAME
 python /create_or_update_comment.py
